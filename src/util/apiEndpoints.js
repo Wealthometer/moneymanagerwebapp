c1b178f8e72b9556d2c596579d73285a9dd8a550
Wealthometer <wealthhajoh87@gmail.com>
2025-12-06 11:42:11 +0100

    Update apiEndpoints.js

--- src/util/apiEndpoints.js
+++ src/util/apiEndpoints.js
@@ -1,1 +1,8 @@
-export const BASE_URL = "http://localhost:8181/api/v1.0"
+// export const BASE_URL = "http://localhost:8181/api/v1.0"
+
+export const BASE_URL = "https://money-manager-api-youtube.onrender.com/api/v1.0";
+
+export const API_ENDPOINTS = {
+  LOGIN: "/login",
+  REGISTER: "/register"
+}
