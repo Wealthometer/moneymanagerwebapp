5a5363b751b103d85491f8154c16cecbd116ede3
Wealthometer <wealthhajoh87@gmail.com>
2025-12-07 07:50:53 +0100

    Update Input.jsx

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,17 +1,23 @@
+import { Eye, EyeClosed } from "lucide-react";
+import { useState } from "react";
+
 const Input = ({ label, value, onChange, placeholder, type }) => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const toggleshowPassword = () => {
+    setShowPassword(!showPassword);
+  };
 
   return (
     <div className="mb-4">
+      <label className="text-[13px] text-slate-800 block mb-1">{label}</label>
 
-      <label className="text-[13px] text-slate-800 block mb-1">
-        {label}  
-      </label>
-      
       <div className="relative">
-        
         <input
           className="w-full bg-transparent outline-none border border-gray-300 rounded py-2 px-3 pr-10 text-gray-700 text-sm leading-tight focus:border-blue-500"
-          type={type}
+          type={
+            type === "password" ? (showPassword ? "text" : "password") : type
+          }
           placeholder={placeholder}
           value={value}
           onChange={(e) => {
@@ -19,10 +25,21 @@ const Input = ({ label, value, onChange, placeholder, type }) => {
           }}
         />
 
+        {type === "password" && (
+          <span
+            className="absolute top-1/2 right-3 transform -translate-y-1/2 cursor-pointer"
+            onClick={toggleshowPassword}
+          >
+            {showPassword ? (
+              <Eye className="text-primary" size={20} onClick={toggleshowPassword} />
+            ) : (
+              <EyeClosed className="text-slate-400" size={20} onClick={toggleshowPassword}/>
+            )}
+          </span>
+        )}
       </div>
-
     </div>
   );
 };
 
-export default Input
+export default Input;
