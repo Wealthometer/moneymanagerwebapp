9ef24ae016d6427c8220d0dae11d06639c7c6b16
Wealthometer <wealthhajoh87@gmail.com>
2025-12-07 10:15:21 +0100

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,14 @@
-import { HomeIcon } from "lucide-react"
+import { HomeIcon } from "lucide-react";
+import Header from "../components/Header";
 
 const Home = () => {
-  return( 
-  <div className="text-3xl font-bold underline"> <HomeIcon/> </div>
-  )
-}
+  return (
+    <div className="text-3xl font-bold underline">
+      <div className="px-4 py-2">
+        <Header />
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
