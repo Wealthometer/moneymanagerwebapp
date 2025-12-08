7dc5ef851458d8e56c5273c6289f8e4df61d6ee6
Wealthometer <wealthhajoh87@gmail.com>
2025-12-08 05:15:54 +0100

    Update apiEndpoints.js

--- src/util/apiEndpoints.js
+++ src/util/apiEndpoints.js
@@ -1,9 +1,11 @@
 // export const BASE_URL = "http://localhost:8181/api/v1.0"
 
 export const BASE_URL = "https://moneymanger-backend.onrender.com/api/v1.0";
+const CLOUDINARY_CLOUD_NAME = "dguyerusv";
 
 export const API_ENDPOINTS = {
   LOGIN: "/login",
-  REGISTER: "/register"
+  REGISTER: "/register",
+  UPLOAD_IMAGE: `https://api.cloudinary.com/v1_1/${CLOUDINARY_CLOUD_NAME}/image/upload`,
 }
 
