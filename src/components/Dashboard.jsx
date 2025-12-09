b693aea002ad6aa0a75eb49710ce2b12a06b6d34
Wealthometer <wealthhajoh87@gmail.com>
2025-12-09 18:31:02 +0100

    Update Dashboard.jsx

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,9 +1,29 @@
+import { useContext } from "react";
 import Menubar from "./Menubar";
+import { AppContext } from "../context/AppContext";
 
 const Dashboard = () => {
+
+    const {user} = useContext(AppContext)
+
     return (
         <div>
             <Menubar  />
+
+            {
+                user && (
+                    <div className="flex">
+                        <div className="max-[1080px]:hidden">
+                            {/* Sidebar Component */}
+                            <div className="text-md text-purple-800">Sidebar</div>
+                        </div>
+
+                        <div className="grow mx-5">
+                            Right
+                        </div>
+                    </div>
+                )
+            }
         </div>
     )
 }
