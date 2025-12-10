e6c2ee90bd1bd56363c8c9a8fb18158c32d08882
Wealthometer <wealthhajoh87@gmail.com>
2025-12-10 05:05:46 +0100

    Update Input.jsx

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,7 +1,15 @@
 import { Eye, EyeClosed } from "lucide-react";
 import { useState } from "react";
 
-const Input = ({ label, value, onChange, placeholder, type }) => {
+const Input = ({
+  label,
+  value,
+  onChange,
+  placeholder,
+  type,
+  isSelect,
+  options,
+}) => {
   const [showPassword, setShowPassword] = useState(false);
 
   const toggleshowPassword = () => {
@@ -13,17 +21,31 @@ const Input = ({ label, value, onChange, placeholder, type }) => {
       <label className="text-[13px] text-slate-800 block mb-1">{label}</label>
 
       <div className="relative">
-        <input
-          className="w-full bg-transparent outline-none border border-gray-300 rounded py-2 px-3 pr-10 text-gray-700 text-sm leading-tight focus:border-blue-500"
-          type={
-            type === "password" ? (showPassword ? "text" : "password") : type
-          }
-          placeholder={placeholder}
-          value={value}
-          onChange={(e) => {
-            onChange(e);
-          }}
-        />
+        {isSelect ? (
+          <select
+            className="w-full bg-transparent outline-none border border-gray-300 rounded-md py-2 px-3 text-gray-700 leading-5 focus::outline-none focus:border-blue-500"
+            value={value}
+            onChange={(e) => onChange(e)}
+          >
+            {options.map((option) => (
+              <option key={option.value} value={option.value}>
+                {option.label}
+              </option>
+            ))}
+          </select>
+        ) : (
+          <input
+            className="w-full bg-transparent outline-none border border-gray-300 rounded py-2 px-3 pr-10 text-gray-700 text-sm leading-tight focus:border-blue-500"
+            type={
+              type === "password" ? (showPassword ? "text" : "password") : type
+            }
+            placeholder={placeholder}
+            value={value}
+            onChange={(e) => {
+              onChange(e);
+            }}
+          />
+        )}
 
         {type === "password" && (
           <span
@@ -31,9 +53,17 @@ const Input = ({ label, value, onChange, placeholder, type }) => {
             onClick={toggleshowPassword}
           >
             {showPassword ? (
-              <Eye className="text-primary" size={20} onClick={toggleshowPassword} />
+              <Eye
+                className="text-primary"
+                size={20}
+                onClick={toggleshowPassword}
+              />
             ) : (
-              <EyeClosed className="text-slate-400" size={20} onClick={toggleshowPassword}/>
+              <EyeClosed
+                className="text-slate-400"
+                size={20}
+                onClick={toggleshowPassword}
+              />
             )}
           </span>
         )}
