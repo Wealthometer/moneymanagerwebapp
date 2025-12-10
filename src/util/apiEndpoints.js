87b5051dba77a387e33e62082e0fe3aba1e17a57
Wealthometer <wealthhajoh87@gmail.com>
2025-12-10 20:40:32 +0100

    Update apiEndpoints.js

--- src/util/apiEndpoints.js
+++ src/util/apiEndpoints.js
@@ -8,5 +8,9 @@ export const API_ENDPOINTS = {
   REGISTER: "/register",
   GET_USER_INFO: "/profile",
   GET_ALL_CATEGORIES: "/categories",
+  ADD_CATEGORY: "/categories",
+  UPDATE_CATEGORY: (categoryId) => `/categories/${categoryId}`,
+  GET_ALL_INCOMES: "/incomes",
+  CATEGORY_BY_TYPE: (type) => `/categories/type${type}`,
   UPLOAD_IMAGE: `https://api.cloudinary.com/v1_1/${CLOUDINARY_CLOUD_NAME}/image/upload`,
 }
