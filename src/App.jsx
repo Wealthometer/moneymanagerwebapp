e9abcdce9e0a7ab7ae2f70305198517f61b656ce
Wealthometer <wealthhajoh87@gmail.com>
2025-12-10 05:05:32 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
 import Income from "./pages/Income";
@@ -15,6 +15,7 @@ function App() {
       <Toaster />
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Root />} />
           <Route path="/dashboard" element={<Home />} />
           <Route path="/income" element={<Income />} />
           <Route path="/expense" element={<Expense />} />
@@ -26,6 +27,15 @@ function App() {
       </BrowserRouter>
     </>
   );
+}
+
+const Root = () => {
+  const isAuthenticated = !!localStorage.getItem("token")
+  return isAuthenticated ? (
+    <Navigate to="/dashboard" />
+  ) : (
+    <Navigate to="/Login" />
+  )
 }
 
 export default App;
