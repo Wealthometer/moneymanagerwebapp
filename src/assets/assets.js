1d8ad7d15df9e272cfc5189cfc5b54ad8195ed14
Wealthometer <wealthhajoh87@gmail.com>
2025-12-10 05:05:34 +0100

    Update assets.js

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,7 +1,42 @@
 import logo from "./logo.png";
 import login_bg from './login_bg.png';
+import { Label } from "recharts";
+import { Coins, FunnelPlus, LayoutDashboard, List, Wallet, Wallet2 } from "lucide-react";
 
 export const assets = {
   logo,
   login_bg
 };
+
+export const SIDE_BAR_DATA = [
+  {
+    id: "01",
+    label: "Dashboard",
+    icon: LayoutDashboard,
+    path: "/dashboard"
+  },
+  {
+    id: "02",
+    label: "Category",
+    icon: List,
+    path: "/category"
+  },
+  {
+    id: "03",
+    label: "Income",
+    icon: Wallet2,
+    path: "/income"
+  },
+  {
+    id: "04",
+    label: "Expense",
+    icon: Coins,
+    path: "/expense"
+  },
+  {
+    id: "05",
+    label: "Filters",
+    icon: FunnelPlus,
+    path: "/filter"
+  },
+]
